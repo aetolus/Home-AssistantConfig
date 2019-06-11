152ca38148041cc3c654503a8b493265e43c8064
aetolus <kyle@tai.net.au>
2019-06-12 03:30:45 +1000

    Update PTV component

--- homeassistant/custom_components/ptv/sensor.py
+++ homeassistant/custom_components/ptv/sensor.py
@@ -1,12 +1,12 @@
 """
-This component provides HA sensor support for the worldtides.info API.
+This component provides HA sensor support for the Public Transport Victoria (PTV) API.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.PTV/
+https://home-assistant.io/components/sensor.ptv/
 """
 import logging
 from datetime import timedelta
-import pytz, dateutil.parser
+import pytz, dateutil.parser, time
 
 import requests
 import voluptuous as vol
@@ -21,7 +21,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = 'PTV'
 
-SCAN_INTERVAL = timedelta(seconds=300)
+SCAN_INTERVAL = timedelta(seconds=60)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -31,9 +31,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the PTV sensor."""
     name = config.get(CONF_NAME)
-
     add_devices([PTVSensor(name)], True)
-
 
 class PTVSensor(Entity):
     """Representation of a PTV sensor."""
@@ -52,24 +50,32 @@ class PTVSensor(Entity):
     def device_state_attributes(self):
         """Return the state attributes of this device."""
         attr = {}
-        train1 = dateutil.parser.parse(self.data['departures'][0]['scheduled_departure_utc'])
-        train1 = train1.astimezone(pytz.timezone("Australia/Melbourne"))
-        train2 = dateutil.parser.parse(self.data['departures'][1]['scheduled_departure_utc'])
-        train2 = train2.astimezone(pytz.timezone("Australia/Melbourne"))
-        train3 = dateutil.parser.parse(self.data['departures'][2]['scheduled_departure_utc'])
-        train3 = train3.astimezone(pytz.timezone("Australia/Melbourne"))
-        attr['next_train'] = train1
-        attr['second_train'] = train2
-        attr['third_train'] = train3
+        x = range(3)
+        try:
+            for i in x:
+                if self.data['departures'][i]['scheduled_departure_utc']:
+                    train_scheduled = dateutil.parser.parse(self.data['departures'][i]['scheduled_departure_utc'])
+                    train_scheduled = train_scheduled.astimezone(pytz.timezone("Australia/Melbourne"))
+                    attr["train{}_scheduled".format(i)] = train_scheduled[11:-9]
+                if self.data['departures'][i]['estimated_departure_utc']:
+                    train_estimated = dateutil.parser.parse(self.data['departures'][i]['estimated_departure_utc'])
+                    train_estimated = train_estimated.astimezone(pytz.timezone("Australia/Melbourne"))
+                    attr["train{}_estimated".format(i)] = train_estimated[11:-9]
+        except:
+            _LOGGER.debug("[ATTR]Departure times unavailable")
         return attr
 
     @property
     def state(self):
         """Return the state of the device."""
         if self.data:
-            next_train = dateutil.parser.parse(self.data['departures'][0]['scheduled_departure_utc'])
-            next_train = next_train.astimezone(pytz.timezone("Australia/Melbourne"))
-            return next_train
+            try:
+                next_train = dateutil.parser.parse(self.data['departures'][0]['scheduled_departure_utc'])
+                next_train = next_train.astimezone(pytz.timezone("Australia/Melbourne"))
+                return next_train[11:-9]
+            except:
+                _LOGGER.debug("[STATE]Next departure time unavailable")
+                return "No departure times currently available"
         return STATE_UNKNOWN
 
     def update(self):
